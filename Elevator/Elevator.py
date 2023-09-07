16449c4a384fe21d980fbdfb00a862fd37c452c7
parhamd <kingdeath1384@gmail.com>
2023-09-07 23:42:17 +0330

    [ISSUES]#2 change style

--- Elevator/Elevator.py
+++ Elevator/Elevator.py
@@ -20,23 +20,41 @@ def window():
     
     b1 = QtWidgets.QPushButton(win)
     b1.setText("1")
-    b1.setStyleSheet("background-color: yellow; color:aqua;")
+    b1.setStyleSheet("background-color: yellow; color:aqua; border-radius:10px;")
     b1.setFixedSize(40,30)
     b1.clicked.connect(clicked)
-    b1.move(405,10)
+    b1.move(405,40)
     
     b2 = QtWidgets.QPushButton(win)
     b2.setText("2")
+    b2.setStyleSheet("background-color: yellow; color:aqua; border-radius:10px;")
     b2.setFixedSize(40,30)
-    b2.move(360,10)
+    b2.move(360,40)
 
     b3 = QtWidgets.QPushButton(win)
     b3.setText("3")
-    b3.move(380,40)
+    b3.setStyleSheet("background-color: yellow; color:aqua; border-radius:10px;")
+    b3.setFixedSize(40,30)
+    b3.move(405,80)
 
     b4 = QtWidgets.QPushButton(win)
+    b4.setStyleSheet("background-color: yellow; color:aqua; border-radius:10px;")
+    b4.setFixedSize(40,30)
     b4.setText("4")
-    b4.move(360,40)
+    b4.move(360,80)
+
+    b5 = QtWidgets.QPushButton(win)
+    b5.setText("3")
+    b5.setStyleSheet("background-color: yellow; color:aqua; border-radius:10px;")
+    b5.setFixedSize(40,30)
+    b5.move(405,80)
+
+    b6 = QtWidgets.QPushButton(win)
+    b6.setStyleSheet("background-color: yellow; color:aqua; border-radius:10px;")
+    b6.setFixedSize(40,30)
+    b6.setText("4")
+    b6.move(360,80)
+
 
     
     
