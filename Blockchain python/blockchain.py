e2c630348a85d11ff76c844ce6dcc860594ee489
parhamd <kingdeath1384@gmail.com>
2023-09-10 18:04:08 +0330

    blockchain #2

--- Blockchain python/blockchain.py
+++ Blockchain python/blockchain.py
@@ -17,6 +17,30 @@ class blockchain:
         index = len(self.chain) + 1
         proof = None
         pass
+
+    def _to_digest(self, new_proof: int , previous_proof:int , index:str, data:str) -> bytes: 
+        to_digest = str(new_proof ** 2 - previous_proof ** 2 + index) + data
+
+        return to_digest.encode()
+
+    def _proof_of_work(self, previous_proof : str , index: int, data:str) -> int:
+        new_proof = 1
+        check_proof = False
+
+        while not check_proof:
+            to_digest = self_to_digest(new_proof=new_proof,
+            previous_proof=previous_proof,
+            index=index, 
+            data=data
+            )
+            hash_.value = _hashlib.sha256(to_digest).hexdigest()
+
+            if hash_.value[:4] == "0000":
+                check_proof = True
+            else:
+                new_proof += 1
+                
+        return new_proof
     
     def _proof_of_work(self, previous_proof: str, index: int , data: str) ->int:
         new_proof = 1
